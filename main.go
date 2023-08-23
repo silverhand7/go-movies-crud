4959ac43de7701465858f5b5a2688e22c332adcd
silverhand7 <refojunior5@gmail.com>
2023-08-23 20:57:31 +0800

    CRUD Movies REST API

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -28,16 +30,16 @@ func getMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
-func deleteMovie(w http.ResponseWriter, r *http.Request) {
+func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for index, movie := range movies {
-		if movie.ID == params["id"] {
-			movies = append(movies[:index], movies[index+1:]...)
-			break
-		}
+	var movie Movie
+	err := json.NewDecoder(r.Body).Decode(&movie)
+	if err != nil {
+		log.Fatal("error decoding movie struct json", err)
 	}
-	json.NewEncoder(w).Encode(movies)
+	movie.ID = strconv.Itoa(rand.Intn(10000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
@@ -48,6 +50,38 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(movie)
 		}
 	}
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var m Movie
+			err := json.NewDecoder(r.Body).Decode(&m)
+			if err != nil {
+				log.Fatal("error decoding movie struct json", err)
+			}
+			m.ID = strconv.Itoa(rand.Intn(10000))
+			movies = append(movies, m)
+			json.NewEncoder(w).Encode(m)
+			return
+		}
+	}
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
 }
 
 func main() {
@@ -83,8 +117,8 @@ func main() {
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	// r.HandleFunc("/movies", createMovies).Methods("POST")
-	// r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 
 	fmt.Println("Starting server at 8080")
